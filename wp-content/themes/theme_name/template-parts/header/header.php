3a113cdc69b325d77e5482e2d034dfa84926870d
dingo-d <denis.zoljom@gmail.com>
2017-05-02 14:32:58 +0200

    Fix escape issues in header and search form

--- wp-content/themes/theme_name/template-parts/header/header.php
+++ wp-content/themes/theme_name/template-parts/header/header.php
@@ -3,8 +3,8 @@
 	$blog_description = get_bloginfo( 'description' );
 ?>
 <div class="header">
-  <a class="header__logo-link" href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_html( $blog_name ); ?>">
-	<img class="header__logo-img" src="<?php echo get_template_directory_uri(); ?>/skin/public/images/logo.svg" title="<?php echo esc_html( $blog_name ); ?> - <?php echo esc_html( $blog_description ); ?>" alt="<?php echo esc_html( $blog_name ); ?> - <?php echo esc_html( $blog_description ); ?>" />
+  <a class="header__logo-link" href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_attr( $blog_name ); ?>">
+	<img class="header__logo-img" src="<?php echo get_template_directory_uri(); ?>/skin/public/images/logo.svg" title="<?php echo esc_attr( $blog_name ); ?> - <?php echo esc_attr( $blog_description ); ?>" alt="<?php echo esc_attr( $blog_name ); ?> - <?php echo esc_attr( $blog_description ); ?>" />
 	</a>
 	<?php
 	bem_menu(
