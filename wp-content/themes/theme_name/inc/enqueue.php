01c0c8da7a4273973f3a38722a9b05feb2e7900a
dingo-d <denis.zoljom@gmail.com>
2017-05-02 11:38:33 +0200

    Fix enqueue file
    Fixed registrating scripts, added fallback for jQuery, fixed localization handle, wpcf7 script enqueue had missing not check in the conditional

--- wp-content/themes/theme_name/inc/enqueue.php
+++ wp-content/themes/theme_name/inc/enqueue.php
@@ -3,37 +3,45 @@
 /**
  * Register scripts
  */
-function register_scripts() {
+add_action( 'wp_enqueue_scripts', 'register_scripts', 1 );
 
+function register_scripts() {
 	// jQuery
-	if ( ! is_admin() ) {
-		// wp_deregister_script( 'jquery-migrate' );
-		wp_deregister_script( 'jquery' );
-		wp_enqueue_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js' );
+
+	// wp_deregister_script( 'jquery-migrate' );
+	wp_deregister_script( 'jquery' );
+
+	$cdn_jQuery = wp_register_script( 'jquery-cdn', 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js' );
+
+	if ( $cdn_jQuery ) {
+		wp_enqueue_script( 'jquery-cdn' );
+	} else {
+		wp_register_script( 'jquery' );
+		wp_enqueue_script( 'jquery' );
 	}
 
 	// JS
-	wp_enqueue_script( 'scripts', get_template_directory_uri() . '/skin/public/scripts/application.js' );
-	if ( function_exists( 'wpcf7_enqueue_scripts' ) ) {
+	wp_register_script( 'scripts', get_template_directory_uri() . '/skin/public/scripts/application.js' );
+	wp_enqueue_script( 'scripts' );
+
+	if ( ! function_exists( 'wpcf7_enqueue_scripts' ) ) {
 		wpcf7_enqueue_scripts();
 	}
 
 	// CSS
-	wp_enqueue_style( 'style', get_template_directory_uri() . '/skin/public/styles/application.css' );
+	wp_register_style( 'style', get_template_directory_uri() . '/skin/public/styles/application.css' );
+	wp_enqueue_style( 'style');
 
 	// Ajax
-	wp_localize_script( 'global_script', 'MyAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+	wp_localize_script( 'scripts', 'MyAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+}
 
-}add_action( 'wp_enqueue_scripts', 'register_scripts', 1 );
-
-// -----------------------------------------------------------
 /**
  * Remove Contact form 7 Includes
  */
 add_filter( 'wpcf7_load_js', '__return_false' );
 add_filter( 'wpcf7_load_css', '__return_false' );
 
-// -----------------------------------------------------------
 /**
 * Remove inline gallery css
 */
