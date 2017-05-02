13daf81e4f39e107ba27fc395ed7186ce7f883b6
dingo-d <denis.zoljom@gmail.com>
2017-05-02 13:53:25 +0200

    Code refactor
    Removed check for add_theme_support

--- wp-content/themes/theme_name/inc/media.php
+++ wp-content/themes/theme_name/inc/media.php
@@ -3,15 +3,14 @@
 /**
  * Set images sizes
  */
-if ( function_exists( 'add_theme_support' ) ) {
-	add_theme_support( 'post-thumbnails' );
+add_theme_support( 'post-thumbnails' );
 
-	// add_image_size( 'medium_thumbnails', 300, 300, true );
-	add_image_size( 'full_width', 1440, 9999, true );
-	add_image_size( 'listing', 570, 320, true );
-	// add_image_size( 'mini_thumbnail', 80, 80, true );
-};
+// add_image_size( 'medium_thumbnails', 300, 300, true );
+add_image_size( 'full_width', 1440, 9999, true );
+add_image_size( 'listing', 570, 320, true );
+// add_image_size( 'mini_thumbnail', 80, 80, true );
 
+add_filter( 'upload_mimes', 'cc_mime_types' );
 /**
  * Enable SVG uplod in media
  */
@@ -19,15 +18,15 @@ function cc_mime_types( $mimes ) {
 	$mimes['svg'] = 'image/svg+xml';
 	return $mimes;
 }
-add_filter( 'upload_mimes', 'cc_mime_types' );
 
+add_filter( 'embed_oembed_html', 'responsive_oembed_filter', 10, 4 );
 /**
  * Wrap utility class arround iframe to enable responsive
+ *
+ * @param  string $html Iframe html to wrap around.
+ * @return  string Wrapped iframe with a utility class.
  */
 function responsive_oembed_filter( $html ) {
 	$return = '<span class="iframe u__embed-video-responsive">' . $html . '</span>';
 	return $return;
 }
-add_filter( 'embed_oembed_html', 'responsive_oembed_filter', 10, 4 );
-
-// -----------------------------------------------------------
