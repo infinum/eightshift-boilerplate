1636529115efbc0832555121f13a78f8888151a8
dingo-d <denis.zoljom@gmail.com>
2017-05-03 09:08:26 +0200

    Add function exists wrapper

--- wp-content/themes/theme_name/inc/media.php
+++ wp-content/themes/theme_name/inc/media.php
@@ -11,22 +11,26 @@ add_image_size( 'listing', 570, 320, true );
 // add_image_size( 'mini_thumbnail', 80, 80, true );
 
 add_filter( 'upload_mimes', 'cc_mime_types' );
-/**
- * Enable SVG uplod in media
- */
-function cc_mime_types( $mimes ) {
-	$mimes['svg'] = 'image/svg+xml';
-	return $mimes;
+if ( ! function_exists( 'cc_mime_types' ) ) {
+	/**
+	 * Enable SVG uplod in media
+	 */
+	function cc_mime_types( $mimes ) {
+		$mimes['svg'] = 'image/svg+xml';
+		return $mimes;
+	}
 }
 
 add_filter( 'embed_oembed_html', 'responsive_oembed_filter', 10, 4 );
-/**
- * Wrap utility class arround iframe to enable responsive
- *
- * @param  string $html Iframe html to wrap around.
- * @return  string Wrapped iframe with a utility class.
- */
-function responsive_oembed_filter( $html ) {
-	$return = '<span class="iframe u__embed-video-responsive">' . $html . '</span>';
-	return $return;
+if ( ! function_exists( 'responsive_oembed_filter' ) ) {
+	/**
+	 * Wrap utility class arround iframe to enable responsive
+	 *
+	 * @param  string $html Iframe html to wrap around.
+	 * @return  string Wrapped iframe with a utility class.
+	 */
+	function responsive_oembed_filter( $html ) {
+		$return = '<span class="iframe u__embed-video-responsive">' . $html . '</span>';
+		return $return;
+	}
 }
