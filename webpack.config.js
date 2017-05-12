d9ec0b17429043a73abaa37419fe28c8c34bcfbb
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2017-05-12 17:22:38 +0200

    Little refactoring and optimisation

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,8 @@ const allModules = {
   rules: [
     {
       test: /\.(js|jsx)$/,
-      loader: 'babel-loader'
+      loader: 'babel-loader',
+      exclude: /node_modules/
     },
     {
       test: /\.json$/,
