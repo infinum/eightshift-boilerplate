83136bdaaa4837c72dc815da2067a84eaef82f62
dingo-d <denis.zoljom@gmail.com>
2017-07-04 10:08:36 +0200

    Fix issue with image urls
    Added global variable for image url and added after setup theme hook in functions.php

--- wp-content/themes/theme_name/functions.php
+++ wp-content/themes/theme_name/functions.php
@@ -17,6 +17,7 @@
  */
 
 define( 'ASSETS_VERSION', '1' );
+define( 'IMAGE_URL', get_template_directory_uri() . '/skin/public/images/' );
 
 /**
  * Global image path
@@ -24,14 +25,18 @@ define( 'ASSETS_VERSION', '1' );
  * @package theme_name
  */
 
-$public_images_path = get_template_directory_uri() . '/skin/public/images/';
+add_action( 'after_setup_theme', 'inf_after_setup_theme_init' );
 
-require get_parent_theme_file_path( '/inc/admin.php' );
-require get_parent_theme_file_path( '/inc/enqueue.php' );
-require get_parent_theme_file_path( '/inc/general.php' );
-require get_parent_theme_file_path( '/inc/helpers.php' );
-require get_parent_theme_file_path( '/inc/media.php' );
-require get_parent_theme_file_path( '/inc/menu.php' );
-require get_parent_theme_file_path( '/inc/pagination.php' );
-require get_parent_theme_file_path( '/inc/theme-options.php' );
-require get_parent_theme_file_path( '/inc/widgets.php' );
+function inf_after_setup_theme_init() {
+
+  require get_parent_theme_file_path( '/inc/admin.php' );
+  require get_parent_theme_file_path( '/inc/enqueue.php' );
+  require get_parent_theme_file_path( '/inc/general.php' );
+  require get_parent_theme_file_path( '/inc/helpers.php' );
+  require get_parent_theme_file_path( '/inc/media.php' );
+  require get_parent_theme_file_path( '/inc/menu.php' );
+  require get_parent_theme_file_path( '/inc/pagination.php' );
+  require get_parent_theme_file_path( '/inc/theme-options.php' );
+  require get_parent_theme_file_path( '/inc/widgets.php' );
+}
+
