34cd5b88892b80cef5f68389b0adf863b972c64a
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2018-01-15 17:14:17 +0100

    creating project based wp config

--- README.md
+++ README.md
@@ -29,26 +29,6 @@ Use `_db-*` scripts to export latest database and uploads folder.
 
 ## wp-config.php
 add this to your `wp-config.php`
-
-```
-// Config
-define( 'AUTOSAVE_INTERVAL', 240 );
-define( 'DISALLOW_FILE_EDIT', true );
-define( 'COMPRESS_CSS', true );
-define( 'COMPRESS_SCRIPTS', true );
-define( 'CONCATENATE_SCRIPTS', true );
-define( 'ENFORCE_GZIP', true );
-define( 'AUTOMATIC_UPDATER_DISABLED', true );
-define( 'WP_POST_REVISIONS', 3 );
-
-define('FORCE_SSL_LOGIN', false); // local false, Staging and production true
-
-// Development
-define('FS_METHOD', 'direct');
-define('WP_DEBUG', true );
-@ini_set('log_errors','On');
-@ini_set('display_errors','Off');
-@ini_set('error_log','error_log.txt');
-```
+// TODO
 
 ------------------------------------
---
+++ wp-config-project.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Global Enviroment variable
+ *
+ * @package theme_name
+ */
+
+// Must be set.
+// Possible options are develop, staging and production.
+define( 'INF_ENV', 'develop' );
+
+if ( ! defined( 'INF_ENV' ) ) {
+  return false;
+}
+
+// Limit revisions for better optimizations.
+define( 'WP_POST_REVISIONS', 3 );
+
+// Optimize assets in admin.
+define( 'COMPRESS_CSS', true );
+define( 'COMPRESS_SCRIPTS', true );
+define( 'CONCATENATE_SCRIPTS', true );
+define( 'ENFORCE_GZIP', true );
+
+// Disable editing theme from admin.
+define( 'DISALLOW_FILE_EDIT', true );
+
+// Auto save interval higher to optimize admin.
+define( 'AUTOSAVE_INTERVAL', 240 );
+
+// Disable automatic updating of plugins.
+define( 'AUTOMATIC_UPDATER_DISABLED', true );
+
+if (INF_ENV === 'develop' ) {
+  // Enable direct upload from admin.
+  define( 'FS_METHOD', 'direct' );
+
+  // Enable debug and error loging.
+  define( 'WP_DEBUG', true );
+  define( 'WP_DEBUG_LOG', true );
+  define( 'WP_DEBUG_DISPLAY', false );
+} else {
+
+  // Disable plugins / themes updates from admin.
+  define( 'DISALLOW_FILE_MODS', true );
+
+  // Force login to admin with ssl.
+  define( 'FORCE_SSL_LOGIN', true );
+}
