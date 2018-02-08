a2eb158c365c23bc29d4bd5f70bbd7b76c81a494
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2018-02-08 10:16:22 +0100

    adding check if template exists and setting name to be in wp naming convention

--- wp-content/themes/init_theme_name/page-templates/page-section-creator.php
+++ wp-content/themes/init_theme_name/page-templates/page-section-creator.php
@@ -13,7 +13,11 @@ $sections = get_field( 'sections' );
 if ( ! empty( $sections ) ) {
   foreach ( $sections as $section ) {
     if ( ! empty( $section ) ) {
-      include( locate_template( 'template-parts/sections/' . $section['acf_fc_layout'] . '.php' ) );
+      $template = locate_template( 'template-parts/sections/section-creator/' . str_replace( '_', '-', $section['acf_fc_layout'] ) . '.php' );
+
+      if ( ! empty( $template ) ) {
+        include( $template );
+      }
     }
   }
 }
