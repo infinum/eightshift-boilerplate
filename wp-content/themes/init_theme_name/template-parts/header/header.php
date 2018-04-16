f54938d821b6ec43460350d1923c071a2497d822
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2018-04-16 17:04:39 +0200

    micro optimisation for header

--- wp-content/themes/init_theme_name/template-parts/header/header.php
+++ wp-content/themes/init_theme_name/template-parts/header/header.php
@@ -9,10 +9,11 @@ use Inf_Theme\Admin\Menu as Menu;
 $menu             = new Menu\Menu();
 $blog_name        = get_bloginfo( 'name' );
 $blog_description = get_bloginfo( 'description' );
+$header_logo_info = $blog_name . ' - ' . $blog_description;
 ?>
 <div class="header">
   <a class="header__logo-link" href="<?php echo esc_url( home_url() ); ?>" title="<?php echo esc_attr( $blog_name ); ?>">
-    <img class="header__logo-img" src="<?php echo esc_url( INF_IMAGE_URL . 'logo.svg' ); ?>" title="<?php echo esc_attr( $blog_name ); ?> - <?php echo esc_attr( $blog_description ); ?>" alt="<?php echo esc_attr( $blog_name ); ?> - <?php echo esc_attr( $blog_description ); ?>" />
+    <img class="header__logo-img" src="<?php echo esc_url( INF_IMAGE_URL . 'logo.svg' ); ?>" title="<?php echo esc_attr( $header_logo_info ); ?>" alt="<?php echo esc_attr( $header_logo_info ); ?>" />
   </a>
   <?php
     echo esc_html( $menu->bem_menu( 'header_main_nav', 'main-navigation' ) );
