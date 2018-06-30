db18cec1457c98cd708ca6d8b24a4ddd02a20a99
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2018-06-30 23:33:28 +0200

    removing favicons in favor of customiser, implmenting cache busting with manifest.json

--- wp-content/themes/inf_theme/template-parts/header/head.php
+++ wp-content/themes/inf_theme/template-parts/header/head.php
@@ -40,5 +40,4 @@
 <link rel="apple-touch-startup-image" href="<?php echo esc_url( INF_IMAGE_URL . 'logo-color.svg' ); ?>">
 
 <?php
-get_template_part( 'template-parts/header/head/favicons' );
 get_template_part( 'template-parts/tracking/head' );
