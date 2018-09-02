34a8bbffcf7c0307b734f299abeacc30d49198b6
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2018-09-02 17:48:31 +0200

    removing media size

--- wp-content/themes/inf_theme/admin/class-media.php
+++ wp-content/themes/inf_theme/admin/class-media.php
@@ -31,7 +31,6 @@ class Media {
    * @since 1.0.0
    */
   public function add_custom_image_sizes() {
-    add_image_size( 'full-width', 1440, 9999, false );
     add_image_size( 'listing', 570, 320, true );
   }
 
