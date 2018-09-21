6bf04f996d2a60ea21ff86eb33ef76e2a2a8e416
Ivan Grginov <ivan.grginov@infinum.hr>
2018-09-21 11:58:34 +0200

    Require files script.

--- bin/rename.js
+++ bin/rename.js
@@ -3,6 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 const prompt = require('prompt-sync')();
+const files = require('./files');
 
 const rootDir = path.join(__dirname, '..');
 
