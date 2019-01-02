2dd41d3e9daafc48ea05108089c27eea05b38c42
Denis Žoljom <denis.zoljom@gmail.com>
2019-01-02 14:52:21 +0100

    Update header.php
    Fix override globals issue.

--- template-parts/header/header.php
+++ template-parts/header/header.php
@@ -8,7 +8,7 @@
 use Inf_Theme\Admin\Menu\Menu;
 use Inf_Theme\Helpers\General_Helper;
 
-$menu             = new Menu();
+$main_menu        = new Menu();
 $blog_name        = get_bloginfo( 'name' );
 $blog_description = get_bloginfo( 'description' );
 $header_logo_info = $blog_name . ' - ' . $blog_description;
@@ -20,7 +20,7 @@ $logo_img         = General_Helper::get_manifest_assets_data( 'logo.svg' );
     <img class="header__logo-img" src="<?php echo esc_url( $logo_img ); ?>" title="<?php echo esc_attr( $header_logo_info ); ?>" alt="<?php echo esc_attr( $header_logo_info ); ?>" />
   </a>
   <?php
-    echo esc_html( $menu->bem_menu( 'header_main_nav', 'main-navigation' ) );
+    echo esc_html( $main_menu->bem_menu( 'header_main_nav', 'main-navigation' ) );
 
     get_template_part( 'template-parts/header/search', 'form' );
   ?>
