7417aff73246f596d4e7b92d47a5e3513dbc796e
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2020-09-11 16:53:27 +0200

    update

--- single.php
+++ single.php
@@ -5,8 +5,6 @@
  * @package EightshiftBoilerplate
  */
 
-use EightshiftBoilerplateVendor\EightshiftLibs\Helpers\Components;
-
 get_header();
 
 if ( have_posts() ) {
@@ -15,8 +13,6 @@ if ( have_posts() ) {
     the_title();
     the_content();
   }
-
-  echo wp_kses_post( Components::render( 'google-rich-snippets' ) );
 }
 
 get_footer();
