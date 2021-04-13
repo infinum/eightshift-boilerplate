816290b52c7ca3b4942886a77a1c06afda3c8641
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2021-04-13 23:39:00 +0200

    removing escape

--- footer.php
+++ footer.php
@@ -13,27 +13,25 @@ use EightshiftBoilerplateVendor\EightshiftLibs\Helpers\Components;
 </main>
 
 <?php
-echo wp_kses_post(
-	Components::render(
-		'layout-three-columns',
-		[
-			'selectorClass' => 'footer',
-			'layoutLeft' => Components::render(
-				'copyright',
-				[
-					'copyrightBy' => esc_html__('Eightshift', 'eightshift-boilerplate'),
-					'copyrightYear' => gmdate('Y'),
-					'copyrightContent' => esc_html__('Made with 🧡  by Eightshift team', 'eightshift-boilerplate'),
-				]
-			),
-			'layoutRight' => Components::render(
-				'menu',
-				[
-					'variation' => 'horizontal'
-				]
-			),
-		]
-	)
+echo Components::render( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+	'layout-three-columns',
+	[
+		'selectorClass' => 'footer',
+		'layoutLeft' => Components::render(
+			'copyright',
+			[
+				'copyrightBy' => esc_html__('Eightshift', 'eightshift-boilerplate'),
+				'copyrightYear' => gmdate('Y'),
+				'copyrightContent' => esc_html__('Made with 🧡  by Eightshift team', 'eightshift-boilerplate'),
+			]
+		),
+		'layoutRight' => Components::render(
+			'menu',
+			[
+				'variation' => 'horizontal'
+			]
+		),
+	]
 );
 
 wp_footer();
