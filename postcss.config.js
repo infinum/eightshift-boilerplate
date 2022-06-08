603f0a1e218a3eaa1fb260abf9dbc76da8ad704f
Ivan Ružević <ivan.ruzevic@infinum.com>
2022-06-08 17:25:45 +0200

    returning post css before rem

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,7 @@
 const autoprefixer = require('autoprefixer');
-const globalManifest = require('./src/Blocks/manifest.json');
-const transformRemsPlugin = require('@eightshift/frontend-libs/scripts/postcss/rem-transforms');
 
-module.exports = ({ file }) => {
-	let plugins = [autoprefixer];
-
-	if (globalManifest?.config?.useRemBaseSize && file.endsWith('/application-blocks-editor.scss')) {
-		plugins = [
-			...plugins,
-			transformRemsPlugin,
-		];
-	}
-
-	return {
-		plugins,
-	};
+module.exports = {
+	plugins: [
+		autoprefixer,
+	],
 };
