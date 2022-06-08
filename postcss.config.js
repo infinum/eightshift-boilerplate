952aff9b1b54de72eca86a320bb508c263205418
Ivan Ružević <ivan.ruzevic@infinum.com>
2022-06-08 17:26:14 +0200

    adding postcss setup

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,18 @@
 const autoprefixer = require('autoprefixer');
+const globalManifest = require('./src/Blocks/manifest.json');
+const transformRemsPlugin = require('@eightshift/frontend-libs/scripts/postcss/rem-transforms');
 
-module.exports = {
-	plugins: [
-		autoprefixer,
-	],
+module.exports = ({ file }) => {
+	let plugins = [autoprefixer];
+
+	if (globalManifest?.config?.useRemBaseSize && file.endsWith('/application-blocks-editor.scss')) {
+		plugins = [
+			...plugins,
+			transformRemsPlugin,
+		];
+	}
+
+	return {
+		plugins,
+	};
 };
