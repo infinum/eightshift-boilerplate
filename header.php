eaee9492a4b790f38ad4513dbe4187836fb39399
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-05-17 15:13:23 +0200

    replace legacy header and footer with partials

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
 
 use EightshiftBoilerplateVendor\EightshiftLibs\Helpers\Components;
 use EightshiftBoilerplate\Manifest\Manifest;
+use EightshiftBoilerplate\AdminMenus\ReusableBlocksHeaderFooter;
 
 ?>
 <!DOCTYPE html>
@@ -28,49 +29,9 @@ use EightshiftBoilerplate\Manifest\Manifest;
 <body <?php body_class(); ?>>
 
 <?php
-// Header Component.
-echo Components::render(
-	'layout-three-columns',
-	[
-		'layoutThreeColumnsHtmlTag' => 'nav',
-		'additionalClass' => 'header',
-		'layoutThreeColumnsLeft' => Components::render(
-			'logo',
-			[
-				'parentClass' => 'header',
-				'logoSrc' => apply_filters(Manifest::MANIFEST_ITEM, 'logo.svg'),
-				'logoAlt' => get_bloginfo('name'),
-				'logoTitle' => get_bloginfo('name'),
-				'logoHref' => get_bloginfo('url'),
-			]
-		),
-		'layoutThreeColumnsRight' => [
-			Components::render(
-				'menu',
-				[
-					'variation' => 'horizontal',
-					'parentClass' => 'header',
-				]
-			),
-			Components::render('hamburger'),
-		]
-	]
-);
-
-// Menu Drawer Style Component.
-echo Components::render(
-	'drawer',
-	[
-		'drawerTrigger' => 'js-hamburger',
-		'drawerMenu' => Components::render(
-			'menu',
-			[
-				'variation' => 'vertical',
-				'parentClass' => 'drawer',
-			]
-		),
-	]
-);
+// Header reusable block.
+$headerPartialId = get_option(ReusableBlocksHeaderFooter::HEADER_PARTIAL) ?? '';
+ReusableBlocksHeaderFooter::renderPartial($headerPartialId);
 ?>
 
 <main class="main-content" id="main-content">
