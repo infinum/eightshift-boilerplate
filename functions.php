13782471addc6a42b70ab554faba80a1b70f2f3a
Ivan Ružević <ivan.ruzevic@infinum.com>
2024-05-08 09:16:44 +0200

    update on few small issues

--- functions.php
+++ functions.php
@@ -3,9 +3,11 @@
 /**
  * Theme Name: %themeName%
  * Description: %description%
- * Author: Team Eightshift
- * Author URI: https://eightshift.com/
- * Version: 12.0.0
+ * Author: %author%
+ * Author URI: %authorUrl%
+ * Version: %version%
+ * License: MIT
+ * License URI: http://www.gnu.org/licenses/gpl.html
  * Text Domain: %textdomain%
  *
  * @package EightshiftBoilerplate
@@ -15,8 +17,6 @@ declare(strict_types=1);
 
 namespace EightshiftBoilerplate;
 
-use EightshiftBoilerplate\Cache\ManifestCache;
-use EightshiftBoilerplate\Main\Main;
 use EightshiftBoilerplateVendor\EightshiftLibs\Cli\Cli;
 
 /**
@@ -29,26 +29,7 @@ if (! \defined('WPINC')) {
 /**
  * Include the autoloader so we can dynamically include the rest of the classes.
  */
-$loader = require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . '/vendor-prefixed/autoload.php';
-
-/**
- * Set all the cache for the theme.
- */
-if (\class_exists(ManifestCache::class)) {
-	(new ManifestCache())->setAllCache();
-}
-
-/**
- * Begins execution of the theme.
- *
- * Since everything within the theme is registered via hooks,
- * then kicking off the theme from this point in the file does
- * not affect the page life cycle.
- */
-if (\class_exists(Main::class)) {
-	(new Main($loader->getPrefixesPsr4(), __NAMESPACE__))->register();
-}
+require __DIR__ . '/vendor/autoload.php';
 
 /**
  * Run all WPCLI commands.
